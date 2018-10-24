045496649a4d5e01a904e9c60a257144b8d5ee35
Mxsxs2 <>
2018-10-25 00:40:37 +0100

    Add models, implement simple rmi calls

---
+++ REST-Server/src/ie/gmit/sw/JAXBDateConverter.java
@@ -0,0 +1,16 @@
+package ie.gmit.sw;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class JAXBDateConverter {
+    public static Date parse(String xmlDateTime) {
+        return javax.xml.bind.DatatypeConverter.parseDateTime(xmlDateTime).getTime();
+    }
+
+    public static String print(Date javaDate) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(javaDate.getTime());
+        return javax.xml.bind.DatatypeConverter.printDateTime(calendar);
+    }
+}
--- REST-Server/src/ie/gmit/sw/server/BookingService.java
+++ REST-Server/src/ie/gmit/sw/server/BookingService.java
@@ -20,13 +20,13 @@ import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 interface BookingService extends Remote {
-        public Booking getBooking(String id) throws RemoteException;
+    public Booking getBooking(String id) throws RemoteException;
 
-        public boolean addBooking(Booking b) throws RemoteException;
+    public boolean addBooking(Booking b) throws RemoteException;
 
-        public boolean changeBooking(Booking b) throws RemoteException;
+    public boolean changeBooking(Booking b) throws RemoteException;
 
-        public ArrayList<Car> getCars(String c) throws RemoteException;
+    public ArrayList<Car> getCars() throws RemoteException;
 
-        public boolean isCarAvailable(String carId, BookingTimeFrame timeFrame) throws RemoteException;
+    public boolean isCarAvailable(String carId, BookingTimeFrame timeFrame) throws RemoteException;
 }
