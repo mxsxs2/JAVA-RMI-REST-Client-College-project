adfe12611481e3e20752344230dbf01a152ad798
Mxsxs2 <>
2018-11-01 00:59:57 +0000

    Start WEB-Client

---
+++ WEB-Client/src/main/java/ie/gmit/sw/webclient/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.webclient;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebclientApplication.class);
+    }
+
+}
---
+++ WEB-Client/src/main/java/ie/gmit/sw/webclient/WebclientApplication.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebclientApplication.class, args);
+    }
+}
